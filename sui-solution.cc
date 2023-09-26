dc8fe1a43bb654542ac9627cc32e4f617a7abaed
Hung Do <hungdojan@gmail.com>
2023-09-26 23:44:16 +0200

    Initial A* algo commit

--- sui-solution.cc
+++ sui-solution.cc
@@ -1,4 +1,5 @@
 #include "search-strategies.h"
+#include <set>
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
 	return {};
@@ -12,6 +13,47 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
     return 0;
 }
 
+double compute_heuristic(const SearchState &state, const AStarHeuristicItf &heuristic);
+
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
+    // the final solution (sequence of actions)
+    std::vector<SearchAction> actions;
+
+    // set of (un)visited states
+    std::vector<SearchState> open;
+    std::set<SearchState> close;
+
+    // g-score and f-score
+    std::map<SearchState, double> gscore;
+    std::map<SearchState, double> fscore;
+
+    // init the structure
+    gscore[init_state] = 0;
+    fscore[init_state] = gscore[init_state] + compute_heuristic(init_state, *heuristic_);
+    open.push_back(init_state);
+
+    // TODO: fix the algorithm
+    while(!open.empty()) {
+        // get state with lowest f-score
+        // f-score is computed as g-score (num_of_moves) + h-score (heuristic function)
+        SearchState s = open.front();
+
+        // check for final state
+        if (s.isFinal()) {
+            return actions;
+        }
+
+        // generate new states through actions
+        // iterate states/actions
+        // calculate current g-score (curr_num_of_moves+1)
+        // if the state has been already discovered, update the g-score
+        // if the new score is lower than existing one -> we can get to this state sooner
+        //
+        // templete functions: compute_heuristic(s, *heuristic_);
+
+        // add new states into open
+        // add current state to close
+        break;
+    }
 	return {};
 }
