005fb34aba37300264338cc67740a09f14c745b3
Hung Do <hungdojan@gmail.com>
2023-10-01 01:50:09 +0200

    The core algorithm

--- sui-solution.cc
+++ sui-solution.cc
@@ -1,119 +1,159 @@
 #include "search-strategies.h"
 #include <set>
 #include <unordered_map>
-#include <unordered_set>
+#include <algorithm>
+#include <iterator>
 
 #if LOGGING
-#define LOG_MOVE(_a)                             \
-  do {                                           \
-    std::cout < < _a << std::endl;               \
-  } while (0)
+#define LOG_MOVE(_a)                                 \
+    do {                                             \
+        std::cout << _a << std::endl;                \
+    } while (0)
 #else
 #define LOG_MOVE(_a) ;
 #endif // LOGGING
 
 #if PRINT_BOARD
-#define LOG_BOARD(_s)                            \
-  do {                                           \
-    std::cout << "[" << _s << "]" << std::endl;  \
-  } while (0)
+#define LOG_BOARD(_s)                                \
+    do {                                             \
+        std::cout << "[" << _s << "]" << std::endl;  \
+    } while (0)
 #else
 #define LOG_BOARD(_s) ;
 #endif // PRINT_BOARD
 
+bool operator==(const SearchState &a, const SearchState &b) {
+    return a.state_ == b.state_;
+}
+
 std::vector<SearchAction>
 BreadthFirstSearch::solve(const SearchState &init_state) {
-  return {};
+    (void)init_state;
+    return {};
 }
 
 std::vector<SearchAction>
 DepthFirstSearch::solve(const SearchState &init_state) {
-  return {};
+    (void)init_state;
+    return {};
 }
 
 double StudentHeuristic::distanceLowerBound(const GameState &state) const {
-  return 0;
+    (void)state;
+    return 0;
 }
 
-double compute_heuristic(const SearchState &state,
-                         const AStarHeuristicItf &heuristic);
+double compute_heuristic(const SearchState &state, const AStarHeuristicItf &heuristic);
 
-void createPath(const std::unordered_map<std::shared_ptr<SearchState>,
-                                         std::shared_ptr<SearchAction>> &moves,
-                const std::shared_ptr<SearchState> &finalState,
-                std::vector<SearchAction> &solution) {
-    // TODO:
-    // std::shared_ptr<SearchState> currentState = finalState;
-    // while(currentState != nullptr) {
+void reconstructPath(std::unordered_map<std::shared_ptr<SearchState>,
+        std::pair<std::shared_ptr<SearchState>, std::shared_ptr<SearchAction>>> &moves,
+        const std::shared_ptr<SearchState> &finalState,
+        std::vector<SearchAction> &solution) {
 
-    // }
+    std::pair<std::shared_ptr<SearchState>,
+              std::shared_ptr<SearchAction>> curr = moves[finalState];
+    // reconstructing the path by back tracking
+    // the map `moves` contains [child_state] -> <parent_state, action>
+    // each parent state is a child state of the successor
+    // we iterate it until we get the first state (parent_state == nullptr)
+    // we collect all the actions and store it in the given `solution` vector
+    while (curr.first != nullptr) {
+        solution.push_back(*curr.second);
+        // solution.insert(solution.begin(), *curr.second);
+        curr = moves[curr.first];
+    }
 }
 
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
-  // the final solution (sequence of actions)
-  std::vector<SearchAction> actions;
+    using namespace std;
+    // set of (un)visited states
+    vector<shared_ptr<SearchState>> open;
+    vector<shared_ptr<SearchState>> close;
 
-  // set of (un)visited states
-  std::vector<std::shared_ptr<SearchState>> open;
-  std::vector<std::shared_ptr<SearchState>> close;
+    // number of moves from start
+    unordered_map<shared_ptr<SearchState>, double> gscore;
+    // total score (gscore + hscore)
+    unordered_map<shared_ptr<SearchState>, double> fscore;
+    // map of transitions [dsc] -> <src, Action>
+    unordered_map<shared_ptr<SearchState>,
+        pair<shared_ptr<SearchState>, shared_ptr<SearchAction>>> moves;
 
-  // g-score and f-score
-  std::unordered_map<std::shared_ptr<SearchState>, double> gscore;
-  std::unordered_map<std::shared_ptr<SearchState>, double> fscore;
-  std::unordered_map<std::shared_ptr<SearchState>,
-                     std::shared_ptr<SearchAction>>
-      moves;
+    // init the structure
+    shared_ptr<SearchState> curr_state = make_shared<SearchState>(init_state);
+    gscore[curr_state] = 0;
+    fscore[curr_state] = compute_heuristic(*curr_state, *heuristic_);
+    open.push_back(curr_state);
+    moves[curr_state] = make_pair(nullptr, nullptr);
 
-  // init the structure
-  gscore[std::make_shared<SearchState>(init_state)] = 0;
-  fscore[std::make_shared<SearchState>(init_state)] =
-      compute_heuristic(init_state, *heuristic_);
-  open.push_back(std::make_shared<SearchState>(init_state));
+    // temp variables
+    double min_fscore, curr_fscore;
+    vector<shared_ptr<SearchState>>::iterator min_it;
 
-#if LOGGING
-  std::cout << "Moves:" << std::endl << std::endl;
-#endif
+    while (!open.empty()) {
+        // get a state with the lowest f-score
+        min_it = open.begin();
+        min_fscore = fscore[*min_it];
 
-  // TODO: fix the algorithm
-  while (!open.empty()) {
-    // get state with lowest f-score
-    // f-score is computed as g-score (num_of_moves) + h-score (heuristic
-    // function)
-    std::shared_ptr<SearchState> s = open.back();
-    open.pop_back();
+        for (auto curr_it = open.begin(); curr_it != open.end(); curr_it++) {
+            curr_fscore = fscore[*curr_it];
+            if (curr_fscore < min_fscore) {
+                min_it = curr_it;
+                min_fscore = curr_fscore;
+            }
+        }
 
-    // check for final state
-    if (s->isFinal()) {
-      // TODO: create path
+        // pop the state with the lowest f-score from the OPEN vector
+        curr_state = *min_it;
+        open.erase(min_it);
 
-      createPath(moves, s, actions);
-      return actions;
+        // check for final state
+        if (curr_state->isFinal()) {
+            // the final solution (sequence of actions)
+            vector<SearchAction> actions;
+            reconstructPath(moves, curr_state, actions);
+            return actions;
+        }
+
+        // iterate over elements
+        for (SearchAction &action : curr_state->actions()) {
+            shared_ptr<SearchState> next_state =
+                make_shared<SearchState>(action.execute(*curr_state));
+            double next_gscore = gscore[curr_state] + 1;
+
+            // check if element has already been discovered
+            auto open_item = find_if(open.begin(), open.end(),
+                    [next_state](const shared_ptr<SearchState> &_s)
+                    { return *_s == *next_state; });
+            auto close_item = find_if(close.begin(), close.end(),
+                    [next_state](const shared_ptr<SearchState> &_s)
+                    { return *_s == *next_state; });
+            if (open_item != open.end()) {   // in open
+                // skip the next state if the path to it
+                // is longer than what currently OPEN offers
+                if (gscore[*open_item] <= next_gscore)
+                    continue;
+                next_state = *open_item;
+            } else if (close_item != close.end()) {          // in close
+                // skip the next state if the path to it
+                // is longer than what currently OPEN offers
+                if (gscore[*close_item] <= next_gscore)
+                    continue;
+
+                // move back from CLOSE to OPEN
+                next_state = *close_item;
+                close.erase(close_item);
+                open.push_back(next_state);
+            } else {                    // not discovered
+                open.push_back(next_state);
+            }
+
+            // calculate score of the neighbours and store the transition between states
+            gscore[next_state] = next_gscore;
+            fscore[next_state] = next_gscore + compute_heuristic(*next_state, *heuristic_);
+            moves[next_state] = make_pair(curr_state, make_shared<SearchAction>(action));
+        }
+        // close states that we have visited
+        close.push_back(curr_state);
     }
-
-    // for (SearchAction &action : s->actions()) {
-    // }
-
-    // #if LOGGING
-    //         std::cout << "------------------------" << std::endl;
-    //         std::cout << "[" << s.actions()[4] << "]" << std::endl;
-    // #if PRINT_BOARD
-    //         std::cout << std::endl << s.actions()[4].execute(s);
-    // #endif
-    //         std::cout << "------------------------" << std::endl;
-    // #endif
-
-    // generate new states through actions
-    // iterate states/actions
-    // calculate current g-score (curr_num_of_moves+1)
-    // if the state has been already discovered, update the g-score
-    // if the new score is lower than existing one -> we can get to this state
-    // sooner
-    //
-    // templete functions: compute_heuristic(s, *heuristic_);
-
-    // add new states into open
-    // add current state to close
-    break;
-  }
-  return {};
+    return {};
 }
