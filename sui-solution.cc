1ae5ad4f6d7c9bb0033e23680b7c83356b7d75aa
Lenka Šoková <lenkasokova04@gmail.com>
2023-10-19 00:12:16 +0200

    Added random access to memory

--- sui-solution.cc
+++ sui-solution.cc
@@ -8,50 +8,55 @@
 
 using namespace std;
 
-struct StateCaller{
+struct StateCallerBFS{
     SearchState state;
     std::shared_ptr<SearchAction> action = nullptr;
-    std::shared_ptr<StateCaller> parentState = nullptr;
+    std::shared_ptr<StateCallerBFS> parentState = nullptr;
 
     int depthLevel = 0;
 
-    bool operator<(const StateCaller& other) const {
+    bool operator<(const StateCallerBFS& other) const {
         return state < other.state;
     }
 };
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
+    if(init_state.isFinal()) return {};
 
-    if(init_state.isFinal())
-        return {};
-
-    set<StateCaller> visitedStates;
-    queue<std::shared_ptr<StateCaller>> openQueue;
+    set<StateCallerBFS> visitedStates;
+    queue<std::shared_ptr<StateCallerBFS>> openQueue;
     vector<SearchAction> solutionActions;
 
-    std::shared_ptr<StateCaller> actualStatePtr;
+    std::shared_ptr<StateCallerBFS> actualStatePtr = std::make_shared<StateCallerBFS>(StateCallerBFS{init_state});
+    std::shared_ptr<StateCallerBFS> newStatePtr;
 
     // init
-    StateCaller actualState = {init_state};
-    visitedStates.insert(actualState);
-    openQueue.push(std::make_shared<StateCaller>(actualState));
+    visitedStates.insert(*actualStatePtr);
+    openQueue.push(actualStatePtr);
 
     while(!openQueue.empty()){
         actualStatePtr = openQueue.front(); openQueue.pop();
 
         // memory watch
-        if (getCurrentRSS() + actualStatePtr->state.actions().size() * sizeof(StateCaller) >= mem_limit_){
+        if (getCurrentRSS() + 50 * 1024 * 1024 >= mem_limit_){
             return {};
         }
 
+        // random access to vector
+        auto actions = std::make_shared<vector<SearchAction>>(actualStatePtr->state.actions());
+        std::vector<int> ind(actions->size());
+        std::iota(ind.begin(), ind.end(), 0);
+        std::random_shuffle(ind.begin(), ind.end());
+
         // check its followers
-        for(const auto& action: actualStatePtr->state.actions()){
-            actualState = {action.execute(actualStatePtr->state), std::make_shared<SearchAction>(action),
-                    actualStatePtr};
+        for(auto& i: ind){
+            std::shared_ptr<SearchAction> action = std::make_shared<SearchAction>((*actions)[i]);
+            newStatePtr = std::make_shared<StateCallerBFS>(StateCallerBFS{action->execute(actualStatePtr->state), action,
+                                                                    actualStatePtr});
 
             // check if the selected element is final
-            if(actualState.state.isFinal()){
-                actualStatePtr = std::make_shared<StateCaller>(actualState);
+            if(newStatePtr->state.isFinal()){
+                actualStatePtr = newStatePtr;
 
                 while(actualStatePtr->parentState != nullptr){
                     solutionActions.emplace_back(*actualStatePtr->action);
@@ -63,12 +68,13 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
             }
 
             // check whether the state is visited
-            if(visitedStates.find(actualState) == visitedStates.end()){
-                visitedStates.insert(actualState);
-                openQueue.push(std::make_shared<StateCaller>(actualState));
+            if(visitedStates.find(*newStatePtr) == visitedStates.end()){
+                visitedStates.insert(*newStatePtr);
+                openQueue.push(newStatePtr);
             }
         }
     }
+
     return {};
 }
 
